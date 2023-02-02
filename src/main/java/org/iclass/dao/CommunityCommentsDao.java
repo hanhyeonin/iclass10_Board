4736abf2bb63dcaf1bb5c80634725a555cac79a3
hanhyeonin <120546586+hanhyeonin@users.noreply.github.com>
2023-02-02 15:30:22 +0900

    기본 CRUD dao 구현 #5

--- src/main/java/org/iclass/dao/CommunityCommentsDao.java
+++ src/main/java/org/iclass/dao/CommunityCommentsDao.java
@@ -1,30 +1,59 @@
 package org.iclass.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
+import org.iclass.vo.Community;
 import org.iclass.vo.CommunityComments;
 
 import mybatis.SqlSessionBean;
 
 public class CommunityCommentsDao {
+
 	private static final CommunityCommentsDao dao = new CommunityCommentsDao();
 	private CommunityCommentsDao() {}
-	public static CommunityCommentsDao getInstance() {
-		return dao;
-	}
-	
+	public static CommunityCommentsDao getInstance() {return dao;}
+
+	// [insert] id="insert" : 댓글 생성
 	public int insert(CommunityComments vo) {
 		SqlSession mapper = SqlSessionBean.getSession();
-		int result = mapper.insert("insert", vo);
-		
+		int result = mapper.insert("communityComments.insert", vo);
 		mapper.commit();
 		mapper.close();
 		return result;
 	}
-	
-	public int delete(CommunityComments vo) {
+
+	// [delete] id="delete" : idx 조회 후 삭제
+	public int delete(int idx) {
 		SqlSession mapper = SqlSessionBean.getSession();
-		int result = mapper.delete("delete", vo);
-		
+		int result = mapper.delete("communityComments.delete", idx);
+		mapper.commit();
+		mapper.close();
+		return result;
+	}
+
+	// [select] id="maxOf"(idx) : idx 최대값
+	public int selectMaxOf() {
+		SqlSession mapper = SqlSessionBean.getSession();
+		int result = mapper.selectOne("communityComments.maxOf");
+		mapper.commit();
+		mapper.close();
+		return result;
+	}
+
+	// [select] id=comments : PK값으로 메인글 조회
+	public List<CommunityComments> comments(long mref) {
+		SqlSession mapper = SqlSessionBean.getSession();
+		List<CommunityComments> list = mapper.selectList("communityComments.comments", mref);
+		mapper.commit();
+		mapper.close();
+		return list;
+	}
+
+	// [select] id="commentsCount" : 메인글 idx의 댓글 갯수
+	public int selectCommentsCount(long mref) {
+		SqlSession mapper = SqlSessionBean.getSession();
+		int result = mapper.selectOne("communityComments.commentsCount",mref);
 		mapper.commit();
 		mapper.close();
 		return result;
--- src/main/java/org/iclass/dao/CommunityDao.java
+++ src/main/java/org/iclass/dao/CommunityDao.java
@@ -14,46 +14,84 @@ public class CommunityDao {
 		return dao;
 	}
 	
-	public int insert(Community vo) {
+	// [update] id=setCommentCount : 댓글 개수 업데이트(댓글 추가, 삭제 할때 필요함.)
+	public int updateCount(long mref) {
 		SqlSession mapper = SqlSessionBean.getSession();
-		int result = mapper.insert("insert", vo);
-		
+		int result = mapper.update("community.setCommentCount",mref);
 		mapper.commit();
 		mapper.close();
 		return result;
 	}
 	
+	// [insert] id=insert : 메인글 생성
+	public long insert(Community vo) {
+		SqlSession mapper = SqlSessionBean.getSession();
+		mapper.insert("community.insert", vo);
+		mapper.commit();
+		mapper.close();
+		return vo.getIdx();		// 매퍼xml에서 selectKey로 시퀀스 값을 vo객체에 저장시킵니다.
+	}
+	
+	// [update] id=update : 글제목, 글내용, ip수정하는 update (PK값으로 조회)
 	public int update(Community vo) {
 		SqlSession mapper = SqlSessionBean.getSession();
-		int result = mapper.update("update", vo);
-		
+		int result = mapper.update("community.update", vo);
 		mapper.commit();
 		mapper.close();
 		return result;
 	}
 	
-	public int delete(Community vo) {
+	// [delete] id=delete : PK값으로 조회 후 삭제
+	public int delete(long idx) {
 		SqlSession mapper = SqlSessionBean.getSession();
-		int result = mapper.delete("delete", vo);
-		
+		int result = mapper.delete("community.delete", idx);
 		mapper.commit();
 		mapper.close();
 		return result;
 	}
 	
-	public Community selectOne(int idx) {
+	// [select] id=selectByIdx : PK값으로 메인글 조회
+	public Community selectByIdx(long idx) {
 		SqlSession mapper = SqlSessionBean.getSession();
-		Community vo = mapper.selectOne("selectOne", idx);
-		
+		Community vo = mapper.selectOne("community.selectByIdx", idx);
+		mapper.commit();
 		mapper.close();
 		return vo;
 	}
 	
-	public List<Community> selectAll(){
+	// [select] id=count : 전체 메인글의 갯수
+	public int selectCount() {
 		SqlSession mapper = SqlSessionBean.getSession();
-		List<Community> list = mapper.selectList("selectAll");
-		
+		int result = mapper.selectOne("community.count");
+		mapper.commit();
+		mapper.close();
+		return result;
+	}
+	
+	
+	// [select] id=maxOf(idx) : idx 최대값
+	public int selectMaxOf() {
+		SqlSession mapper = SqlSessionBean.getSession();
+		int result = mapper.selectOne("community.maxOf");
+		mapper.commit();
+		mapper.close();
+		return result;
+	}
+	
+	// [select] id="select10" : 게시판 글목록은 idx 의 내림차순, rownum(임의생성) 10개씩 조회 
+	public List<Community> list(){
+		SqlSession mapper = SqlSessionBean.getSession();
+		List<Community> list = mapper.selectList("community.list");
+		mapper.commit();
 		mapper.close();
 		return list;
+	}
+	
+	public int setReadCount(long idx) {
+		SqlSession mapper = SqlSessionBean.getSession();
+		int result = mapper.update("community.setReadCount", idx);
+		mapper.commit();
+		mapper.close();
+		return result;
 	}
 }
